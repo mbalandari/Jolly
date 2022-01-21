e497c70c5c1fd33ef53afbb97cded48e1afac918
mbalandari <m.balandari.toroghi@gmail.com>
2022-01-21 12:38:25 +0300

    add images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { useState } from 'react';
 import './App.css';
+import one from './img/1.jpg';
+import two from './img/2.jpg';
+import three from './img/3.jpg';
+import four from './img/4.jpg';
+import five from './img/5.jpg';
+import six from './img/6.jpg';
+import seven from './img/7.jpg';
+
+
+
+const images = [one, two, three, four, five, six, seven];
 
 function App() {
+  const [currentImage, setCurrentImage] = useState(0);
+
   return (
     <div className="App">
       <div className='title'>
@@ -8,7 +22,7 @@ function App() {
         <h2>A photography project <br /> by Maz & Chasseur</h2>
       </div>
       <div className="image-container">
-        <img src="" alt="" />
+        <img src={images[0]} alt="Chasseur" />
       </div>
     </div>
   );
